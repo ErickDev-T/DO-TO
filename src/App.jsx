3abdaccf029cef06b881936cb8e3d079acce255a
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-12-16 23:26:10 -0400

    avance de la conecion a la db

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 
 import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
+const API_URL = "http://localhost/DO-TO/src/api.php";
 function App() {
+
+  const [usuarios, setUsuarios] = useState([]);
+  const [formData, setFormData] = useState({ nombre: "", email: "", telefono: "" });
+
+// consumienso api con axios
+
 
 
   return (
